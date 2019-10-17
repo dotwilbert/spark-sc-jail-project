c3b75662277e304d797a988a4a99bfbc137819b6
dotwilbert <dotwilbert@protonmail.com>
2019-10-17 16:25:01 -0700

    Add historic load

--- dag-sc-jail-load-historic-files.py
+++ dag-sc-jail-load-historic-files.py
@@ -27,13 +27,7 @@ default_args = {
 # Every day schedule will run at +2/+3hrs after publication. Container in UTC, publication in Pacific
 dag = DAG('sc-jail-daily-population-count-sheet',
           default_args=default_args,
-          schedule_interval='0 14 * * *',
-          user_defined_filters={
-              'iso8601': lambda t: t.to_iso8601_string(),
-              'to_pacific_date': lambda utc_iso8601: pendulum.parse(utc_iso8601)
-              .in_timezone('America/Los_Angeles')
-              .format('YYYYMMDD', formatter='alternative')
-          },
+          schedule_interval='@once',
           dagrun_timeout=timedelta(minutes=5),
           )
 
@@ -49,27 +43,6 @@ generate_load_id_task = PythonOperator(
     provide_context=True
 )
 
-dl_task = BashOperator(
-    task_id='dl_population_sheet',
-    bash_command='source /home/airflow/.conda_environment;\
-        conda activate airflow-project;\
-        /home/airflow/scripts/sc-jail-project/dl-santa-clara-dpcs.py\
-        -o /bigdata/{{ execution_date | iso8601 | to_pacific_date }}-santa-clara-daily-population-sheet.pdf',
-    params={'retries': 3},
-    dag=dag,
-)
-
-sheet_to_text_task = BashOperator(
-    task_id='sheet_to_text',
-    bash_command='source /home/airflow/.conda_environment; conda activate airflow-project; \
-    /home/airflow/scripts/sc-jail-project/convert-dpcs-to-text.py \
-    -i /bigdata/{{ execution_date | iso8601 | to_pacific_date }}-santa-clara-daily-population-sheet.pdf \
-    -o /bigdata/{{ execution_date | iso8601 | to_pacific_date }}-santa-clara-daily-population-sheet.txt \
-    --keep-infile \
-    --keep-imagefile',
-    dag=dag,
-)
-
 load_with_spark_task = BashOperator(
     task_id='load_with_spark',
     bash_command="source /home/airflow/.conda_environment; conda activate airflow-project; \
@@ -78,7 +51,7 @@ load_with_spark_task = BashOperator(
     --driver-class-path /usr/share/java/postgresql.jar \
     --jars local:///usr/share/java/postgresql.jar \
     /home/airflow/scripts/sc-jail-project/load-dpcs.py \
-    -g '/bigdata/{{ execution_date | iso8601 | to_pacific_date }}-santa-clara-daily-population-sheet.txt' \
+    -g '/bigdata/*-santa-clara-daily-population-sheet.txt' \
     -u $SC_JAIL_USER \
     -p $SC_JAIL_PASSWORD \
     -s $SC_JAIL_DB \
@@ -95,9 +68,7 @@ staging_to_tables_task = BashOperator(
     dag=dag,
 )
 
-dl_task >> sheet_to_text_task >> load_with_spark_task
-generate_load_id_task >> load_with_spark_task
-load_with_spark_task >> staging_to_tables_task
+generate_load_id_task >> load_with_spark_task >> staging_to_tables_task
 
 if __name__ == "__main__":
     dag.cli()
